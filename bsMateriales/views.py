e448b8ede7f7ed444665af280b2972717b8b8b3a
pablo1n7 <pablo1n7@gmail.com>
2012-11-19 20:15:44 -0300

    Nuevo Proyecto de Mejora

---
+++ bsMateriales/views.py
@@ -0,0 +1,6 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+    """docstring for index"""
+    return render_to_response('index.html',context_instance=RequestContext(request)) 
