471fb2dde2b39b48c0311d394f1d522977cf24e7
pablo1n7 <pablo1n7@gmail.com>
2013-09-16 11:21:27 -0300

    corregido: detalles de la factura duplicado por producto

--- bsMateriales/utiles.py
+++ bsMateriales/utiles.py
@@ -11,6 +11,21 @@ class AdaptadorFactura():
     fecha = None
     total = None
 
+    def setDetalles(self, detalles):
+        diccionario = {}
+        for detalle in detalles:
+            if diccionario.has_key(detalle.producto):
+                    d = diccionario.get(detalle.producto)
+                    d.cantidad = d.cantidad + detalle.cantidad
+                    d.subtotal=  d.subtotal+ detalle.subtotal
+                    del diccionario[detalle.producto]
+                    diccionario[detalle.producto]= d
+            else:
+                diccionario[detalle.producto] = detalle 
+
+        self.detalles = diccionario.values()
+
+
     def inicializar(self, notaVenta):
     	self.factura = Factura.objects.get(ventaNota = notaVenta)
     	self.pk = self.factura.pk
@@ -19,7 +34,7 @@ class AdaptadorFactura():
     	self.ventaNota = self.factura.ventaNota
     	self.apellidoCliente = self.factura.ventaNota.apellidoCliente
     	self.nombreCliente = self.factura.ventaNota.nombreCliente
-    	self.detalles = DetalleFactura.objects.filter(factura = self.factura)
+    	self.setDetalles( DetalleFactura.objects.filter(factura = self.factura))
 
     
 
